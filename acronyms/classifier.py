1b6addc97b134d03e3efce1d7a96000a7ef18602
paulius <danpaulius@gmail.com>
2021-05-08 00:58:52 +0300

    Abbreviation detection experiment

---
+++ acronyms/classifier.py
@@ -0,0 +1,145 @@
+import os
+import joblib
+from typing import Dict
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import VotingClassifier
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.utils.class_weight import compute_class_weight
+from catboost.core import CatBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost.sklearn import XGBClassifier
+
+SEED = 42
+
+def catboost_classifier(train_data, labels):
+    clf = CatBoostClassifier(verbose=False)
+    clf.fit(train_data, labels)
+    return clf
+
+def random_forest_classifier(train_data, labels):
+    clf = RandomForestClassifier(n_estimators=200)
+    clf.fit(train_data, labels)
+    return clf
+
+def voting_classifier(X_train, y_train):
+    major = X_train[y_train==0]
+    minor = X_train[y_train==1]
+    kf = KFold(n_splits=10, random_state=SEED, shuffle=True)
+    classifiers = []
+    i = 1
+    for ind, train_index in kf.split(major):
+        print(f'Training classifier for subsample {i}')
+        df_sub = np.vstack([major[train_index], minor])
+        y_sub = [0] * len(train_index) + [1] * minor.shape[0]
+        clf = catboost_classifier(df_sub, y_sub)
+        classifiers.append((f'clf{i}', clf))
+        i += 1
+    clf = VotingClassifier(estimators=classifiers, voting='soft')
+    clf.fit(X_train, y_train)
+    return clf
+
+def weighted_catboost_classifier(train_data, labels):
+    classes=np.unique(labels)
+    weights = compute_class_weight(class_weight='balanced', classes=classes, y=labels)
+    class_weights = dict(zip(classes, weights))
+    clf = CatBoostClassifier(verbose=False, class_weights=class_weights)
+    clf.fit(train_data, labels)
+    return clf
+
+def plain_catboost_classifier(train_data, y_train, weighted=False):
+    X_train = train_data.copy()
+    X_train.fillna(value='NA', inplace=True)
+    X_train = X_train.astype('category')
+    params = {'learning_rate': [0.03, 0.1],
+            'depth': [4, 6, 10],
+            'l2_leaf_reg': [1, 3, 5, 7, 9]}
+    kwargs = {}
+    if weighted is True:
+        classes=np.unique(y_train)
+        weights = compute_class_weight(class_weight='balanced', classes=classes, y=y_train)
+        class_weights = dict(zip(classes, weights))
+        kwargs = {'class_weights': class_weights}
+    cb = CatBoostClassifier(verbose=False, cat_features=train_data.columns.tolist(), **kwargs)
+    cb.grid_search(params, X=X_train, y=y_train)
+    return cb
+
+def xgb_classifier(train_data, labels):
+    clf = XGBClassifier(learning_rate=0.01, n_estimators=200, silent=True, nthread=1)
+    params = {
+        'min_child_weight': [1, 5, 10],
+        'gamma': [0.5, 1, 1.5, 2, 5],
+        'subsample': [0.6, 0.8, 1.0],
+        'colsample_bytree': [0.6, 0.8, 1.0],
+        'max_depth': [3, 4, 5]
+    }
+    search = GridSearchCV(clf, param_grid=params, cv=2, n_jobs=-1)
+    search.fit(train_data, labels)
+    return search.best_estimator_
+
+
+def preprocess_data(X_train, y_train):
+    train_data = X_train.copy()
+    labels_train = y_train.copy()
+    label_encoder = LabelEncoder().fit(labels_train)
+    labels = label_encoder.transform(labels_train)
+    categorical_features_mask = (train_data.dtypes == bool) | (train_data.dtypes == object)
+    categorical_features = train_data.columns[categorical_features_mask]
+
+    def encode_column(train_data, col):
+        train_data.loc[pd.isnull(train_data[col]), col] = 'N/A'
+        encoder = LabelEncoder()
+        train_data[col] = encoder.fit_transform(train_data[col])
+        return encoder
+
+    column_encoders = dict()
+    ohe = None
+    if len(categorical_features) > 0:
+        column_encoders = {x: encode_column(train_data, x) for x in categorical_features}
+        ohe = ColumnTransformer([('encoder', OneHotEncoder(handle_unknown='ignore'), categorical_features_mask)],
+                                remainder='passthrough', sparse_threshold=0)
+        train_data = ohe.fit_transform(train_data)
+    return train_data, labels, column_encoders, ohe, label_encoder
+
+def preprocess_test_data(X_test, y_test, column_encoders: Dict[str, LabelEncoder], ohe: ColumnTransformer, label_encoder: LabelEncoder):
+    def apply_encoder(encoder, data):
+        enc_dict = dict(zip(encoder.classes_, encoder.transform(encoder.classes_)))
+        return data.apply(lambda x: enc_dict.get(x, 999999))  # Encode "unseen" values
+    test_data = X_test.copy()
+    labels_test = label_encoder.transform(y_test)
+    categorical_features = column_encoders.keys()
+    if len(categorical_features) > 0:
+        for x in categorical_features:
+            test_data[x] = apply_encoder(column_encoders[x], test_data[x])
+        test_data = ohe.transform(test_data)
+    return test_data, labels_test
+
+
+def test_classifier(clf, X_test, y_test, column_encoders: Dict[str, LabelEncoder], ohe: ColumnTransformer, label_encoder: LabelEncoder):
+    test_data, labels_test = preprocess_test_data(X_test, y_test, column_encoders, ohe, label_encoder)
+    results = clf.predict(test_data).astype(int)
+    results = pd.DataFrame({'predicted': label_encoder.inverse_transform(results), 'original': labels_test})
+    try:
+        probs = pd.DataFrame(clf.predict_proba(test_data), columns=['Class_{}'.format(i) for i in set(labels_test)])
+        results = pd.concat([results.reset_index(drop=True), probs.reset_index(drop=True)], axis='columns')
+    except AttributeError:
+        pass
+    return results.reset_index(drop=True)
+
+
+def train_classifier(X_train, y_train, output_dir: str='.', X_test=None, y_test=None, classifier=catboost_classifier):
+    train_data, labels, column_encoders, ohe, label_encoder = preprocess_data(X_train, y_train)
+    categorical_features = list(column_encoders.keys())
+    clf = classifier(train_data, labels)
+    if output_dir is not None:
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        joblib.dump(clf, os.path.join(output_dir, 'model.joblib'))
+        joblib.dump((categorical_features, label_encoder, column_encoders, ohe),
+                    os.path.join(output_dir, 'preprocess.joblib'))
+    results = None
+    if X_test is not None:
+        results = test_classifier(clf, X_test, y_test, column_encoders, ohe, label_encoder)
+    return clf, results, ohe.get_feature_names()
---
+++ acronyms/dataset.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import itertools
+import string
+import pandas as pd
+import numpy as np
+import enchant
+import nltk
+from nltk.tokenize import wordpunct_tokenize
+from tqdm import tqdm
+import torch
+import stanza
+
+NLTK_PATH = '/mnt/DATA/data/nltk'
+STANZA_DIR = os.path.join('..', 'stanza_resources')
+nltk.data.path.append(NLTK_PATH)
+tqdm.pandas()
+torch.set_default_tensor_type(torch.FloatTensor)
+stanza.download('en', dir=STANZA_DIR)
+tagger = stanza.Pipeline('en', dir=STANZA_DIR, processors='tokenize,pos', tokenize_pretokenized=True)
+wchecker = enchant.Dict("en_US")
+
+
+def try_tokenize(tok):
+    try:
+        return wordpunct_tokenize(tok)
+    except:
+        return []
+
+def abbrev_candidates(items):
+    return list(filter(lambda x: 1 < len(x) <= 5, set(itertools.chain(*map(try_tokenize, items)))))
+
+def word2ngrams(text, n=3):
+  return [text[i:i+n] for i in range(len(text)-n+1)]
+
+def create_features(candidates, entries):
+
+    def word_features(input_):
+        tok, context = input_
+        doc = tagger([context])
+        tagged = [(word.text, word.upos) for sent in doc.sentences for word in sent.words]
+        tok_ind = [i for i, item in enumerate(tagged) if item[0] == tok]
+        tok_ind = tok_ind[0]
+        return {
+            'token': tok,
+            'has.vowels': len(set(tok.lower()) & set(tuple('aeiouy'))) > 0,
+            'length': len(tok),
+            'has.special': len(set(tok) & set(tuple('&.'))) > 0,
+            'just.letters': tok.isalpha(),
+            'all.upper': tok.isupper(),
+            'roman': set('xvi').issuperset(tok.lower()),
+            'english.word': wchecker.check(tok.lower()),
+            'long.char.seq': any(len(set(a))==1 for a in word2ngrams(tok.lower(), n=3)),
+            'starts.with.two': [len(set(a)) for a in word2ngrams(tok.lower(), n=2)][0] == 1,
+            'pos': tagged[tok_ind][1],
+            'prev.pos': tagged[tok_ind-1][1] if tok_ind > 0 else None,
+            'prev.pos2': tagged[tok_ind-2][1] if tok_ind > 1 else None,
+            'next.pos': tagged[tok_ind+1][1] if tok_ind < len(tagged)-1 else None,
+            'next.pos2': tagged[tok_ind+2][1] if tok_ind < len(tagged)-2 else None
+        }
+
+    contexts = [(x,y) for y in entries for x in candidates if x in y]
+    return pd.DataFrame(data=list(map(word_features, contexts)))
+
+data = pd.read_csv('bpmn_dataset_cleaned.csv', sep=';')
+items = data.groupby('Model').agg({'Lane': list, 'Task': list})
+items['entries'] = items.apply(lambda row: set(row['Lane'] + row['Task']), axis=1)
+items['candidates'] = items['entries'].apply(abbrev_candidates)
+items['tokenized'] = items['entries'].apply(lambda row: list(map(try_tokenize, row)))
+features = pd.concat(items.progress_apply(lambda row: create_features(row['candidates'], row['tokenized']), axis=1).tolist()).reset_index(drop=True)
+features = features[~features['token'].str.match('^\w\d+$')]  # Remove entries like A1, M10, etc.
+valid_token = lambda x: len(set(x) & set(string.ascii_letters)) > 1   # At least two letters
+features = features[features['token'].apply(valid_token)]
+features['label'] = np.where(features['all.upper'] == True, 1, 0)     # Initial labelling, remove from features during classification if not changed!
+features.to_csv('dataset.csv', index=False)
